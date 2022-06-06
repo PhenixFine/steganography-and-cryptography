0ab9ed769993d48978968d949608c60a7c14c19c
Danny <64429863+PhenixFine@users.noreply.github.com>
2022-06-06 17:04:49 -0400

    Stage 2 of 4 for JetBrains Academy - Kotlin - Steganography and Cryptography - project.
     Main.kt - updates
     • runCommand(String) - adjusted to call hide() function
     • hide() - using helper functions getFile(), getBufferedImage(), and saveImage(), it gets the input and output image file names, updates the image with the least significant bit for each color is set to 1, if a problem arises during the process an appropriate error message is printed.
     • getFile(type: String) - gets the type of file needed from the user using the function getString()
     • getBufferedImage(File) - creates a buffered image from the passed file, if an issue arises from the creation null is returned.
     • saveImage(BufferedImage, File) - if the saving of the BufferedImage is successful true is returned otherwise false is returned.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
 fun main() {
     var command = getCommand()
 
@@ -12,10 +16,44 @@ private fun getCommand() = getString("Task (hide, show, exit):")
 
 private fun runCommand(command: String) {
     when (command) {
-        "hide" -> "Hiding message in image."
+        "hide" -> hide()
         "show" -> "Obtaining message from image."
         else -> "Wrong task: $command"
     }.let { println(it) }
+}
+
+private fun hide(): String {
+    val input = getFile("Input")
+    val output = getFile("Output")
+    val image = getBufferedImage(input) ?: return "Can't read input file!"
+    val inPath = "Input image: ${input.invariantSeparatorsPath}\n"
+    val outPath = "Output image: ${output.invariantSeparatorsPath}\n"
+    val message = inPath + outPath + "Image ${output.name} is saved."
+
+    for (i in 0 until image.width) {
+        for (j in 0 until image.height) {
+            image.setRGB(i, j, image.getRGB(i, j) or 0x010101)
+        }
+    }
+    return if (saveImage(image, output)) message else "Can't write to output file!"
+}
+
+private fun getFile(type: String) = File(getString("$type image file:"))
+
+private fun getBufferedImage(file: File): BufferedImage? {
+    return try {
+        ImageIO.read(file)
+    } catch (e: Exception) {
+        null
+    }
+}
+
+private fun saveImage(image: BufferedImage, file: File): Boolean {
+    return try {
+        ImageIO.write(image, "png", file)
+    } catch (e: Exception) {
+        false
+    }
 }
 
 private fun getString(message: String): String {
