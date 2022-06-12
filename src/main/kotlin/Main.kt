8f5dda0c3d3c0e89530360ec757e8e3c88ac03fb
Danny <64429863+PhenixFine@users.noreply.github.com>
2022-06-12 17:31:17 -0400

    Stage 3 of 4 for JetBrains Academy - Kotlin - Steganography and Cryptography - project.
     Main.kt -
     • added constant INPUT_ERROR String for when there is a problem with reading a file
     • added list END for marking the end of the users message within an image.
     • runCommand() - updated so that when the user types `show` the function show() is called
     • hide() - updated
         • updated to use const INPUT_ERROR
         • gets the user message from getBits()
         • checks if there is enough space in image to store user message. If not, the user is informed that there isn't and if so the message is stored in the image
         • lambda `save` was created to save the buffered image to file, as the need to save might be called from within the for loops or outside it ( for cases when space required for the user message would take up all the space available ).
         • the for loops were adjusted to where the outer for loop goes through image height and inner for loop goes through its width.
         • within the for loop the user message is stored bit by bit within the color blue's least significant bit.
     • show() - loops through the image saving the least significant bit of the color blue, and converting each eight bits encountered to a char to save within the String message. If the end of the image is reached without encountering the END marker that indicates the end of a message, the user is informed that no message was hidden. If the END marker is encountered the saved message is presented to the user.
     • getBits() - using getString() it requests the message from the user. The message is then mapped to a list as it's Char code and combined with the END list, each of which is then converted to a binary String padded with leading zeroes to make each one of length eight. All binary strings are then combined as one String and returned.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
+import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+
+private const val INPUT_ERROR = "Can't read input file!"
+private val END = listOf(0, 0, 3)
 
 fun main() {
     var command = getCommand()
@@ -17,7 +21,7 @@ private fun getCommand() = getString("Task (hide, show, exit):")
 private fun runCommand(command: String) {
     when (command) {
         "hide" -> hide()
-        "show" -> "Obtaining message from image."
+        "show" -> show()
         else -> "Wrong task: $command"
     }.let { println(it) }
 }
@@ -25,17 +29,49 @@ private fun runCommand(command: String) {
 private fun hide(): String {
     val input = getFile("Input")
     val output = getFile("Output")
-    val image = getBufferedImage(input) ?: return "Can't read input file!"
-    val inPath = "Input image: ${input.invariantSeparatorsPath}\n"
-    val outPath = "Output image: ${output.invariantSeparatorsPath}\n"
-    val message = inPath + outPath + "Image ${output.name} is saved."
+    val image = getBufferedImage(input) ?: return INPUT_ERROR
+    val userMessage = getBits()
+    val enoughSpace = image.width * image.height >= userMessage.length
+    val notLargeEnough = "The input image is not large enough to hold this message."
+    val message = if (enoughSpace) "Message saved in ${output.name} image." else return notLargeEnough
+    val save = { if (saveImage(image, output)) message else "Can't write to output file!" }
+    var index = 0
 
-    for (i in 0 until image.width) {
-        for (j in 0 until image.height) {
-            image.setRGB(i, j, image.getRGB(i, j) or 0x010101)
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            if (index == userMessage.length) return save()
+            val bit = userMessage[index++].digitToInt()
+            val color = Color(image.getRGB(x, y)).let { Color(it.red, it.green, it.blue.and(254).or(bit)) }
+            image.setRGB(x, y, color.rgb)
         }
     }
-    return if (saveImage(image, output)) message else "Can't write to output file!"
+    return save()
+}
+
+private fun show(): String {
+    val input = getFile("Input")
+    val image = getBufferedImage(input) ?: return INPUT_ERROR
+    var lastThree = listOf(1, 1, 1)
+    var lastEight = ""
+    var message = ""
+
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
+            lastEight += Color(image.getRGB(x, y)).blue % 2
+            if (lastEight.length == 8) {
+                val code = lastEight.toInt(2)
+                message += code.toChar()
+                lastThree = lastThree.drop(1) + code
+                if (lastThree == END) return "Message:\n" + message.dropLast(3)
+                lastEight = ""
+            }
+        }
+    }
+    return "No message was hidden!"
+}
+
+private fun getBits() = (getString("Message to hide:").map { it.code } + END).joinToString("") {
+    it.toString(2).padStart(8, '0')
 }
 
 private fun getFile(type: String) = File(getString("$type image file:"))
